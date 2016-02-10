c8e0cf4411cefd3ae9f3ed4ac42db12290998e2d
sopgenorth <sopgenorth@gmail.com>
2016-02-09 21:36:59 -0600

    Better PID gains, gain scheduling and ability to change gains from the surface

--- rovPID.cpp
+++ rovPID.cpp
@@ -4,13 +4,16 @@
 #include "rovCOM.h"
 
 #define PID_LOOP_RATE_MS 7
+#define PID_GAIN_SWITCH_POINT (1500)
+int32_t PIDgainSwitchPoint_uBar = PID_GAIN_SWITCH_POINT;
 
 //Define variables we'll be connecting PID to
 double depthSetpoint, depthInput, depthOutput;
 int32_t zThrusters;
 
 //Specify the links and initial tuning parameters for PID
-int32_t Kp=85000, Ki=79000, Kd=65000;
+int32_t Kp=215000, Ki=68500, Kd=88000;
+int32_t Kp_low=270, Ki_low=350, Kd_low=150;
 PID depthPID(&depthInput, &depthOutput, &depthSetpoint, Kp/1000.0f, Ki/1000.0f, Kd/1000.0f, REVERSE);
 
 MS5837 depthSensor;
@@ -38,32 +41,56 @@ void rovPIDinit(){
  */
 unsigned long previousTime = 0;
 int32_t prevPID_setPoint = 0;
+boolean highGains = true;
 boolean rovPIDrun(){
-  if(inGroup.PID_setPoint != 0 && inGroup.PID_setPoint != prevPID_setPoint){
-    prevPID_setPoint = inGroup.PID_setPoint;
-    depthSetpoint = (double) inGroup.PID_setPoint;
-  }
-  
-  if(inGroup.PID_Kp != 0 && inGroup.PID_Kp != Kp){
-    Kp = inGroup.PID_Kp;
-    depthPID.SetTunings(Kp/1000.0f, Ki/1000.0f, Kd/1000.0f);
-  }
-  if(inGroup.PID_Ki != 0 && inGroup.PID_Ki != Ki){
-    Ki = inGroup.PID_Ki;
-    depthPID.SetTunings(Kp/1000.0f, Ki/1000.0f, Kd/1000.0f);
-  }
-  if(inGroup.PID_Kd != 0 && inGroup.PID_Kd != Kd){
-    Kd = inGroup.PID_Kd;
-    depthPID.SetTunings(Kp/1000.0f, Ki/1000.0f, Kd/1000.0f);
-  }
-  
-  boolean updatedValues = false;
-  
   //get new depth data
   depthSensor.read(); //takes about 4.7ms to get a new reading, non-blocking
   
   //update sensor data
     depthInput = depthSensor.pressure();
+  
+  if(inGroup.PID_setPoint != 0 && inGroup.PID_setPoint != prevPID_setPoint){
+    prevPID_setPoint = inGroup.PID_setPoint;
+    depthSetpoint = (double) inGroup.PID_setPoint;
+  }
+    
+  if((inGroup.PIDgainSwitchPoint_uBar != 0 && inGroup.PIDgainSwitchPoint_uBar != PIDgainSwitchPoint_uBar)){
+    PIDgainSwitchPoint_uBar = inGroup.PIDgainSwitchPoint_uBar;
+  }
+  
+  if(abs(depthInput - depthSetpoint) > (PIDgainSwitchPoint_uBar/1000.0f)){ //far away from setpoint, use the high gains
+    if(!highGains || (inGroup.PID_Kp != 0 && inGroup.PID_Kp != Kp)){
+      Kp = inGroup.PID_Kp;
+      depthPID.SetTunings(Kp/1000.0f, Ki/1000.0f, Kd/1000.0f);
+    }
+    if(!highGains || (inGroup.PID_Ki != 0 && inGroup.PID_Ki != Ki)){
+      Ki = inGroup.PID_Ki;
+      depthPID.SetTunings(Kp/1000.0f, Ki/1000.0f, Kd/1000.0f);
+    }
+    if(!highGains || (inGroup.PID_Kd != 0 && inGroup.PID_Kd != Kd)){
+      Kd = inGroup.PID_Kd;
+      depthPID.SetTunings(Kp/1000.0f, Ki/1000.0f, Kd/1000.0f);
+    }
+    highGains = true;
+  }
+  else //we're pretty close to the setpoint, use lower gains
+  {
+    if(highGains || (inGroup.PID_Kp_low != 0 && inGroup.PID_Kp_low != Kp_low)){
+      Kp_low = inGroup.PID_Kp_low;
+      depthPID.SetTunings(Kp_low/1000.0f, Ki_low/1000.0f, Kd_low/1000.0f);
+    }
+    if(highGains || (inGroup.PID_Ki_low != 0 && inGroup.PID_Ki_low != Ki_low)){
+      Ki_low = inGroup.PID_Ki_low;
+      depthPID.SetTunings(Kp_low/1000.0f, Ki_low/1000.0f, Kd_low/1000.0f);
+    }
+    if(highGains || (inGroup.PID_Kd_low != 0 && inGroup.PID_Kd_low != Kd_low)){
+      Kd_low = inGroup.PID_Kd_low;
+      depthPID.SetTunings(Kp/1000.0f, Ki/1000.0f, Kd/1000.0f);
+    }
+    highGains = false;
+  }
+  
+  boolean updatedValues = false;
   
   //update depthPID every PID_LOOP_RATE_MS
   unsigned long currentTime = millis();
