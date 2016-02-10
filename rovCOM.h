c347be711478db32607c041c2ee953d84cbbdf14
sopgenorth <sopgenorth@gmail.com>
2016-02-09 21:32:40 -0600

    ROV communication update
    - use sizeof to determine struct size instead of relying on the user
    - added a bunch of new variables

--- rovCOM.h
+++ rovCOM.h
@@ -5,18 +5,23 @@
 #include <Ethernet.h>
 #include <EthernetUdp.h>
 
-#define IN_NUM 13
 //data received from PC
 struct inNames_t{
   int32_t m0,m1,m2,m3,m4,m5,m6,m7; //8 motors/thrusters, speed range: -3200 to 3200
   int32_t PID_Kp, PID_Ki, PID_Kd, PID_dir, PID_setPoint;
+  int32_t PID_Kp_low, PID_Ki_low, PID_Kd_low;
+  int32_t PIDgainSwitchPoint_uBar;
+  int32_t depthFilter;
 };
 
-#define OUT_NUM 3
 //data to send to PC
 struct outNames_t{
-  int32_t depth_microBar, rovTempMilliCelsius, PID_output;
+  int32_t depth_microBar, depth_microBarRaw, rovTempMilliCelsius, PID_output, rovCycleTime_us;
 };
+
+// total number of variables in each of the structs
+static const int32_t OUT_NUM = sizeof(outNames_t)/4;
+static const int32_t IN_NUM = sizeof(inNames_t)/4;
 
 extern inNames_t inGroup;
 extern outNames_t outGroup;
